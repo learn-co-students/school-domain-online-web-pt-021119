8e180347a7928613979f31645b9364826593155b
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-27 18:16:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School 
+  #attr_reader :add_student
+  attr_accessor :roster
+  
+  def initialize(name, *roster)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name #k/v pair exists, so adding to name array
+    else 
+      @roster[grade] = [name] #k/v pair doesn't exist yet, so creating grade with new array containing names
+    end
+  end
+  
+  def grade(grade)
+    @roster.each do |grade_k, names_v|
+      if grade_k == grade 
+        return names_v
+      end
+    end
+  end
+  
+  def sort 
+    @roster.each do |grade_k, names_v|
+      names_v.sort! #use .sort! so it sorts the array given, rather than .sort which would return a new, sorted array
+    end
+    return @roster
+  end
+  
+end
