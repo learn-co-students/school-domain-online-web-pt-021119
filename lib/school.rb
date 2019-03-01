4027f9f211d80fd5611e23f9b7b57dc2c0a6522e
lieby weintraub <liebahinda@gmail.com>
2019-03-01 06:28:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_reader :roster
+  def initialize(name)
+    @name=name
+    @roster={}
+  end
+  def add_student(name, grade)
+   if @roster.has_key?(grade) == false
+     @roster[grade]=[] 
+     @roster[grade] << name
+   else 
+     @roster[grade]  << name
+  end
+end 
+def grade(grade_number)
+  @roster[grade_number]
+end 
+def sort 
+  second_hash = {}
+    @roster.each do |y, z|
+      second_hash[y] = z.sort
+    end
+    second_hash
+end
+end 
