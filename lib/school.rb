383cfc2208b09d384ef5c8a5f416592c948271b9
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-01 21:55:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School 
+  
+  def initialize(school)
+    @school = school
+    @roster = {} 
+  end 
+  
+  def school 
+    @school 
+  end
+  
+  def roster 
+    @roster 
+  end 
+  
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << name 
+    else 
+      @roster[grade] << name
+    end 
+    @roster 
+  end 
+  
+  def grade(grade)
+    @roster.fetch(grade)
+  end 
+  
+  def sort
+    @roster.each{|grade, students| roster[grade] = students.sort}
+  end 
+  
+end 
+
