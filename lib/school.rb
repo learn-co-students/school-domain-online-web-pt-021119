facd5d59ffac47a268ade32de1f5502a995c2d34
chilbert <chase@chasehilbert.com>
2019-03-02 15:56:41 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
+require 'pry'
 # code here!
+
+class School
+attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    roster_sorted = {}
+    @roster.each do |grade, name|
+        roster_sorted[grade] = name.sort
+      end
+      roster_sorted
+  end
+
+end
