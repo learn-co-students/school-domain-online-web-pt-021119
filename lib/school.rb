076254170543b64652d62ba885c8965dc2402ef4
Malcolm Leake <echelon1089@gmail.com>
2019-03-04 05:11:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+class School
+      attr_accessor :roster
+
+  def initialize(name)
+  @name = name 
+  @roster = {}
+  end
+  
+  def add_student(add_student, grade)
+    if @roster.has_key?(grade)
+    @roster[grade] << add_student
+    else
+  @roster[grade] = []
+      @roster[grade] << add_student
+      end
+  end 
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash =  {}
+    @roster.each do |grade, students|
+      new_hash[grade] = students.sort
+    end
+  new_hash
+  end
+
+
+
+end
