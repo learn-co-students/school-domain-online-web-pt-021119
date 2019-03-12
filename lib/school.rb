6efddde3bbbac5515a6a271c9c7ceab57c299fa7
Jay Sokolowski <jaysoko@gmail.com>
2019-03-12 00:44:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] unless @roster[grade]
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def name(grade)
+    @roster[grade]
+  end
+
+  def sort_pre_refactor
+    sorted = {}
+    @roster.each do|key, values|
+      sorted[key] ||= values.sort
+    end
+    sorted
+  end
+
+  def sort
+    @roster.map {|k, v| v.sort!}
+    @roster
+  end
+
+end
+
